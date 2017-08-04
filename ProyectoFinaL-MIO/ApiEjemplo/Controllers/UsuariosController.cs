7a07e83903721bf2651993579dac4e88ae8bfb2c
SANTIAGO MATIAS RAVAGLIA <42660700@ort>
2017-08-04 08:49:24 -0300

    .

---
+++ ProyectoFinaL-MIO/ApiEjemplo/Controllers/UsuariosController.cs
@@ -0,0 +1,53 @@
+﻿using ApiEjemplo.Data;
+using ApiEjemplo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace ApiEjemplo.Controllers
+{
+    public class UsuariosController : ApiController
+    {
+        [ResponseType(typeof(Usuarios))]
+        public IHttpActionResult Post(Usuarios oUsuario)
+        {
+            if (oUsuario == null || string.IsNullOrEmpty(oUsuario.Nombre) || string.IsNullOrEmpty(oUsuario.Apellido))//validamos nombre(validar todoooo)
+            {
+                return BadRequest("Datos incorrectos.");
+            }
+            try
+            {
+                UsuariosData.Insert(oUsuario);
+
+            }
+            catch (Exception e)
+            {
+
+                return BadRequest("Se ha producido un error al intentar realizar la consulta.");
+            }
+            return Ok();
+        }
+
+        public IList<Usuarios> Get()
+        {
+            return UsuariosData.ObtenerTodoDeUsuarios();
+        }
+
+
+        [ResponseType(typeof(IList<Usuarios>))]
+        public IHttpActionResult Get(string Nombre)
+        {
+            List<Usuarios> lista = new List<Usuarios>();
+            lista = UsuariosData.ObtenerPorNombre(Nombre);
+            if (lista.Count == 0)
+            {
+                return NotFound();
+            }
+            return Ok(lista);
+        }
+    }
+}
