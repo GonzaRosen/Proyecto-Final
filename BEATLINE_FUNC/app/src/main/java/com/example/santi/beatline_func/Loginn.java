804895e9af5fe12255a3e43141e0e39a790ea77a
SantiRavaglia <santi23062000@gmail.com>
2017-08-24 20:46:29 -0300

    a

--- BEATLINE_FUNC/app/src/main/java/com/example/santi/beatline_func/Loginn.java
+++ BEATLINE_FUNC/app/src/main/java/com/example/santi/beatline_func/Loginn.java
@@ -1,27 +1,18 @@
-﻿package com.example.santi.beatline_func;
+package com.example.santi.beatline_func;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.CalendarView;
-import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.Spinner;
 import android.widget.Toast;
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+
 import java.io.IOException;
-import java.security.PrivateKey;
-import java.security.PublicKey;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
 
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
@@ -29,68 +20,37 @@ import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 
-public class RegistracionUsuario extends Activity {
+public class Loginn extends AppCompatActivity {
 
-    EditText Nombre;
-    EditText Apellido;
-    EditText Usuario;
-    CalendarView FechaNac;
     EditText Email;
-    EditText Pass;
-
-    CheckBox Terminos;
+    EditText Password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.layout_regusu);
-
-            Terminos = (CheckBox)findViewById(R.id.ChkBox);
-            Nombre = (EditText)findViewById(R.id.Nombre);
-            Apellido = (EditText)findViewById(R.id.Apellido);
-            Usuario = (EditText)findViewById(R.id.Usuario);
-            FechaNac = (CalendarView)findViewById(R.id.FechaNac);
-            Email = (EditText)findViewById(R.id.Email);
-            Pass = (EditText)findViewById(R.id.Contraseña);
-
-
-        }
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loginn);
+        Email = (EditText)findViewById(R.id.Email);
+        Password = (EditText)findViewById(R.id.Contraseña);
     }
 
-        public void Volver (View view)
-    {
-        Intent Activity;
-        Activity = new Intent(this,MainActivity.class);
-        startActivity(Activity);
-    }
+    public void Login (View view) {
+        String Mail = Email.getText().toString();
+        String Contraseña = Password.getText().toString();
+        Boolean Coincide;
 
-    public void Grabar (View view) {
-
-        String Edit1 = Nombre.getText().toString();
-        String Edit2 = Apellido.getText().toString();
-        String Edit3 = Usuario.getText().toString();
-        String Edit4 = Email.getText().toString();
-        String Edit5 = Pass.getText().toString();
-
-        if(!(Edit1.equals("") || Edit2.equals("") || Edit3.equals("") || Edit4.equals("") || Edit5.equals("")))
+        if(!(Mail.equals("") || Contraseña.equals("")))
         {
-            if (Terminos.isChecked())
+            if (Incorrecto(Coincide) == false)
             {
-                String urlApi = "http://thebealineproject.azurewebsites.net/api/usuarios/post";
+                String urlApi = "http://thebealineproject.azurewebsites.net/api/usuarios/APILOGIN";
                 Persona p = new Persona();
-                p.setNombre(Edit1);
-                p.setApellido(Edit2);
-                p.setEmail(Edit4);
-                //p.setFechaNac(FechaNac.getDate());
-                p.setUsuario(Edit3);
-                p.setContraseña(Edit5);
-
+                p.setEmail(Mail);
+                p.setContraseña(Contraseña);
                 GsonBuilder builder = new GsonBuilder();
                 Gson gson = builder.create();
                 System.out.println(gson.toJson(p));
 
-                new ConectarAPITask().execute("POST",urlApi, gson.toJson(p));
+                new Loginn.ConectarAPITask().execute("POST",urlApi, gson.toJson(p));
 
                 Intent Activity;
                 Activity = new Intent(this,MainActivity.class);
@@ -99,14 +59,14 @@ public class RegistracionUsuario extends Activity {
             else
             {
                 Toast toast1;
-                toast1 = Toast.makeText(RegistracionUsuario.this, "Debe aceptar los terminos y condiciones para completar la registración", Toast.LENGTH_SHORT);
+                toast1 = Toast.makeText(Loginn.this, "Usuario y/o contraseña incorrectos.", Toast.LENGTH_SHORT);
                 toast1.show();
             }
         }
         else
         {
             Toast toast1;
-            toast1 = Toast.makeText(RegistracionUsuario.this, "No deje campos en blanco", Toast.LENGTH_SHORT);
+            toast1 = Toast.makeText(Loginn.this, "No deje campos en blanco.", Toast.LENGTH_SHORT);
             toast1.show();
         }
     }
@@ -118,7 +78,6 @@ public class RegistracionUsuario extends Activity {
         protected void onPostExecute(Persona persona) {
             super.onPostExecute(persona);
             super.onPostExecute(persona);
-            //Log.d("ope :",persona.getNombre());
             if (persona != null) {
 
             }
@@ -193,5 +152,10 @@ public class RegistracionUsuario extends Activity {
 
         }
 
+    }
+
+    public boolean Incorrecto(boolean NoCoincide)
+    {
+        return NoCoincide;
     }
 }
