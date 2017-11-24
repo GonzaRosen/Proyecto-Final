5f9310a41196bb60932bb8a2cfd792fb67351536
GONZALO EZEQUIEL ROSEN <42496630@ort>
2017-11-24 12:32:34 -0300

    .

--- ProyectoFinaL-MIO/ApiEjemplo/Data/UHasUData.cs
+++ ProyectoFinaL-MIO/ApiEjemplo/Data/UHasUData.cs
@@ -10,9 +10,8 @@ namespace ApiEjemplo.Data
     {      
         public static void InsertarUHU(int IdUsuario, int IdSeguido)
         {
-            string sInsert = string.Format("Insert into tusuarios_has_tusuarios (" + IdUsuario + "," + IdSeguido + ") " +
-                "values ({0},{1})"
-                    );
+            string sInsert = "Insert into tusuarios_has_tusuarios (" + IdUsuario + "," + IdSeguido + ") " +
+                "values ({0},{1})";
             DBHelper.EjecutarIUD(sInsert);
         }
     }
