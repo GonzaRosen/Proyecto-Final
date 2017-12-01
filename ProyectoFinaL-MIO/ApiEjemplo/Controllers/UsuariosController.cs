c408aeb845af7e5049f9a850e8c471ad57335ead
SANTIAGO MATIAS RAVAGLIA <42660700@ort>
2017-12-01 10:34:32 -0300

    .

--- ProyectoFinaL-MIO/ApiEjemplo/Controllers/UsuariosController.cs
+++ ProyectoFinaL-MIO/ApiEjemplo/Controllers/UsuariosController.cs
@@ -30,6 +30,7 @@ namespace ApiEjemplo.Controllers
             return Ok();
         }
 
+        [HttpPut]
         public IHttpActionResult Put(Usuarios oUsuario)
         {
             UsuariosData.Update(oUsuario);
